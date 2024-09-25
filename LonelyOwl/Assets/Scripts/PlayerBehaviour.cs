780631d788b44ce539098cc571058ab9be0ea969
Adhithya Narayanan <65213873+adhithya5599@users.noreply.github.com>
2024-09-25 12:46:02 -0600

    changed movement

--- LonelyOwl/Assets/Scripts/PlayerBehaviour.cs
+++ LonelyOwl/Assets/Scripts/PlayerBehaviour.cs
@@ -110,9 +110,9 @@ public class PlayerBehaviour : MonoBehaviour
 
     void Movement()
     {
-        vertical = Input.GetAxisRaw("Horizontal");
-        horizontal = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if (direction.magnitude >= 0.1f)
         {

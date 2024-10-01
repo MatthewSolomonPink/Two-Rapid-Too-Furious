d24044b6e68a62493fdf5bf6e5ecb45dce9ef6c4
Adhithya Narayanan <65213873+adhithya5599@users.noreply.github.com>
2024-10-01 17:02:36 -0600

    added player to hidden list in cover page

--- LonelyOwl/Assets/Scripts/AnimationScript.cs
+++ LonelyOwl/Assets/Scripts/AnimationScript.cs
@@ -1,33 +1,51 @@
+using System.Collections;
 using UnityEngine;
 
 public class AnimationScript : MonoBehaviour
 {
     Animator animator;
+
     [SerializeField] GameObject bookCutOut;
     [SerializeField] GameObject bookText;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject player;
+    [SerializeField] float waitForAnimation;
     void Start()
     {
         animator = GetComponent<Animator>();
-        if (animator != null)
-        {
-            bookCutOut.SetActive(false);
-            bookText.SetActive(false);
-            animator.Play(0);
-            
-        }
-
+        
+        PlayBookOpenAnimation();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //Debug.Log(animator.GetBool("isCompleted"));
-        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !animator.IsInTransition(0))
+        //Check if animation is completed
+        if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 
+            && !animator.IsInTransition(0))
         {
-            bookCutOut.SetActive(true);
-            bookText.SetActive(true);
+            StartCoroutine(TranistionEffect());
+            SetGameObjectStatus(true);
             this.gameObject.SetActive(false);
         }
+    }
+    private void PlayBookOpenAnimation()
+    {
+        if (animator != null)
+        {
+            SetGameObjectStatus(false);
+            animator.Play(0);
+
+        }
+    }
+    private void SetGameObjectStatus(bool isVisible)
+    {
+        player.SetActive(isVisible);
+        bookCutOut.SetActive(isVisible);
+        bookText.SetActive(isVisible);
+    }
+
+    //Added for smooth after animation effect
+    IEnumerator TranistionEffect()
+    {
+        yield return new WaitForSeconds(waitForAnimation);
     }
 }

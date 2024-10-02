9ba92de1eed1c0e6c3d2ae72bef70ed9b65d723f
BrokenPottery <80779420+BrokenPottery@users.noreply.github.com>
2024-10-02 15:39:00 -0600

    Stage 2 working and cool :)

--- LonelyOwl/Assets/HootCheckStage2.cs
+++ LonelyOwl/Assets/HootCheckStage2.cs
@@ -9,11 +9,16 @@ public class HootCheckStage2 : MonoBehaviour
     //[SerializeField] private GameObject player;
     [SerializeField] private AudioClip m_AudioClip;
     [SerializeField] private float waitTime = 2;
+    [SerializeField] private float speed = 2;
     [SerializeField] private GameObject manager;
+    [SerializeField] private GameObject objects;
+    [SerializeField] private TransitionBehavior transition;
+
+    private float yVal = 0;
 
     private AudioSource AudioSource;
-    private bool fired = false;
 
+    private bool fired = false;
     private bool owlReady = false;
 
     private void Start()
@@ -35,7 +40,7 @@ public class HootCheckStage2 : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Player");
+        if (other.gameObject.tag == "Player")
         {
             owlReady = false;
         }
@@ -49,19 +54,38 @@ public class HootCheckStage2 : MonoBehaviour
             fired = true;
             StartCoroutine(nextStage(waitTime));
         }
+
+        if (fired)
+        {
+            yVal += Time.deltaTime * speed;
+            if (yVal > 6.06f)
+                yVal = 6.06f;
+            objects.transform.position = new Vector3(objects.transform.position.x, yVal, objects.transform.position.z);
+        }
     }
 
     public IEnumerator nextStage(float secondsToWait)
     {
-        if (m_AudioClip != null) AudioSource.PlayOneShot(m_AudioClip);
+        yield return new WaitForSecondsRealtime(.5f);
+        if (m_AudioClip != null) 
+        { 
+            AudioSource.PlayOneShot(m_AudioClip);
+            yield return new WaitForSecondsRealtime(.5f);
+            AudioSource.PlayOneShot(m_AudioClip);
+            yield return new WaitForSecondsRealtime(.2f);
+            AudioSource.PlayOneShot(m_AudioClip);
+        }
         Stage2EventHandler levelEvent = manager.GetComponent<Stage2EventHandler>();
         levelEvent.playerInteracted = true;
 
         yield return new WaitForSecondsRealtime(secondsToWait);
 
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        /*int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         Debug.Log("Load Next Scene");
-        SceneManager.LoadSceneAsync(currentSceneIndex + 1);
+        SceneManager.LoadSceneAsync(currentSceneIndex + 1);*/
+
+        
+        transition.goToNextScene();
     }
 
 }

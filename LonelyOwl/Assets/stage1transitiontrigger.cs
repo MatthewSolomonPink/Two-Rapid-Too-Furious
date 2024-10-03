ad4d6b03cbbd750def56591c3016b2df04243c20
BrokenPottery <80779420+BrokenPottery@users.noreply.github.com>
2024-10-02 23:44:06 -0600

    Weird Maybe broken push

--- LonelyOwl/Assets/stage1transitiontrigger.cs
+++ LonelyOwl/Assets/stage1transitiontrigger.cs
@@ -6,10 +6,13 @@ public class stage1transitiontrigger : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Hello");
         if (collision.gameObject.tag == "Player")
         {
             transition.goToNextScene();
             Debug.Log("Hello");
         }
     }
+
+   
 }
